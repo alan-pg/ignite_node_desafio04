fd9421e631ba8b1483f7e42c55d8014e30af3527
Alan Goncalves <alan92goncalves@gmail.com>
2022-04-18 19:29:08 -0300

    success

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userEmailExists = this.usersRepository.findByEmail(email);
+    if (userEmailExists) {
+      throw new Error("Email already exists");
+    }
+    const createdUser = this.usersRepository.create({ name, email });
+    return createdUser;
   }
 }
 
